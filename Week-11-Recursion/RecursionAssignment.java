d24c460017a69ac0d2824e1c17f8cc3fa7c9e630
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-05-07 00:26:15 +0530

    3 questions completed

--- Week-11-Recursion/RecursionAssignment.java
+++ Week-11-Recursion/RecursionAssignment.java
@@ -1,7 +1,62 @@
+
+
 public class ResursionAssignment {
 
-    // Q1 : Given an integer, find out the sum of its digits using recursion.
+    public static void main(String[] args) {
+    
+        // Q1 : Given an integer, find out the sum of its digits using recursion.
+        int num = 128;
+        System.out.println("The sum is: " + sumOfDigits(num));
 
-    
-    
+        /*
+        Q.2 - Given a number n. Find the sum of natural numbers till n but with alternate signs.
+        That means if n = 5 then you have to return 1-2+3-4+5 = 3 as your answer.
+        Constraints : 0<=n<=1e6
+        */
+
+        int n = 5;
+
+        boolean isMinus = false;
+        int sum = 0;
+        for(int i = 1; i<=n; i++) {
+            isMinus = !isMinus;
+            if(isMinus) {
+                sum += i;
+            } else {
+                sum -= i;
+            }
+        }
+        System.out.println("The sum with alternate operators of n is: " + sum);
+
+        /*
+        Q.3 - Print the max value of the array [ 13, 1, -3, 22, 5].
+        */
+        int[] arr = {13,1,-3,22,5};
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum number in this array is: " + max);
+
+        
+
+    }
+
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        if(num == 0) {
+            return 0;
+        } 
+        int result = num%10 + sumOfDigits(num/10);
+        return result;
+
+
+    }
+
 }
+
+
+
