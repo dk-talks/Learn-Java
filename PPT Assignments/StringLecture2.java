0f544e7d842ac29b8f1b50b1801e9321559a8943
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-07-03 23:41:31 +0530

    Update StringLecture2.java

--- PPT Assignments/StringLecture2.java
+++ PPT Assignments/StringLecture2.java
@@ -15,6 +15,61 @@ public class StringLecture2 {
         boolean isMorphic = isIsomorphic(s, t);
         System.out.println("The strings are isomorphic? " + isMorphic);
 
+
+        /*-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        Que 2 - Strobogrammatic Number
+         */
+
+         String num = "69";
+         boolean isStrobogrammatic = isStrobogrammatic(num);
+         System.out.println("Is num Strobogrammatic? " + isStrobogrammatic);
+
+
+         /*-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        Que 3. 415. Add Strings
+         */
+
+         String num1 = "123";
+         String num2 = "11";
+
+         String addition = addStrings(num1, num2);
+         System.out.println("Addition of two strings is: " + addition);
+
+
+        /*-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        Que 4 - 557. Reverse Words in a String III
+         */
+
+        String s4  = "Let's take LeetCode contest";
+        String reverseS4 = reverseWords(s4);
+        System.out.println("Reversed words in a string is: " + reverseS4);
+
+
+        
+        /*-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        Que 5. - 541. Reverse String II
+         */
+
+        String s5 = "abcdefg";
+        int k = 2;
+
+        String reversedS5 = reverseString(s5, k);
+        System.out.println("Reversed string in groups is : " + reversedS5);
+
+
+        /*-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        Que 6. - 796. Rotate String
+         */
+
+        String s6 = "abcde";
+        String goal = "cdeab";
+
+        boolean isRotated = rotateString(s6, goal);
+        System.out.println("Is s rotated: " + isRotated);
+
+
+
+
     }
 
     public static boolean isIsomorphic(String s, String t) {
@@ -44,6 +99,151 @@ public class StringLecture2 {
         }
         return true;
         
+
+    }
+
+    public static boolean isStrobogrammatic(String num) {
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('8', '8');
+        int l = 0, r = num.length() - 1;
+        while (l <= r) {
+            if (!map.containsKey(num.charAt(l))) return false;
+            if (map.get(num.charAt(l)) != num.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+
+
+    public static String addStrings(String num1, String num2) {
+        
+        StringBuilder result = new StringBuilder();
+
+        int carry = 0;
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+
+        while(i >= 0 || j >= 0) {
+
+            int digit1 = 0;
+            int digit2 = 0;
+            if(i>=0) {
+                digit1 = num1.charAt(i) - '0';
+            }
+            if(j>=0) {
+                digit2 = num2.charAt(j) - '0';
+            }
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum/10;
+            sum = sum%10;
+
+            result.append(sum);
+
+            i--;
+            j--;
+
+        }
+
+        if(carry != 0) {
+            result.append(carry);
+        }
+
+        return result.reverse().toString();
+
+    }
+
+
+    public static String reverseWords(String s) {
+
+        // split the string based on space in an arrya
+        String[] arr = s.split(" ");
+
+        // now loop throught this array and on the go reverse each element
+
+        for(int i = 0; i<arr.length; i++) {
+            String currentStr = arr[i];
+            char[] currArr = currentStr.toCharArray();
+            for(int j = 0; j<currArr.length/2; j++) {
+                // revese the currArr
+                char left = currArr[j];
+                currArr[j] = currArr[currArr.length-j-1];
+                currArr[currArr.length-j-1] = left;
+            }
+            arr[i] = new String(currArr);
+
+        }
+
+        String resultStr = String.join(" ", arr);
+        return resultStr;
+        
+    }
+
+    public static String reverseString(String s, int k) {
+        char[] charArray = s.toCharArray();
+        int i = 0;
+        
+        while (i < charArray.length) {
+            int start = i;
+            int end = Math.min(i + k - 1, charArray.length - 1);
+            
+            while (start < end) {
+                char temp = charArray[start];
+                charArray[start] = charArray[end];
+                charArray[end] = temp;
+                start++;
+                end--;
+            }
+            
+            i += 2 * k;
+        }
+        
+        return new String(charArray);
+    }
+
+    public static boolean rotateString(String s, String goal) {
+
+        if(s.length() != goal.length()) {
+            return false;
+        }
+
+        // optimized approach
+
+        String concatenated = s + s;
+        if(concatenated.contains(goal)) {
+            return true;
+        }
+        return false;
+
+        // brute force approach below
+        
+        /*
+        char[] sArr = s.toCharArray();
+        int rotationCount = 0;
+
+        while(rotationCount < sArr.length) {
+            // rotate the array 
+            for(int i = 0; i<sArr.length-1; i++) {
+                char left = sArr[i];
+                sArr[i] = sArr[i+1];
+                sArr[i+1] = left;
+            }
+            // now check if after this rotation, is s equal to goal
+            String newS = new String(sArr);
+            if(newS.equals(goal)) {
+                return true;
+            }
+            rotationCount++;
+        }
+        return false;
+         */
 
     }
     
