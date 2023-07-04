1321c7ea41e53b9ad18afd2ff0ba962a51d07dd1
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-07-05 01:51:48 +0530

    Create Question5.java

---
+++ PPT Assignments/MockTest2/Question5.java
@@ -0,0 +1,17 @@
+public class Question5 {
+    public static void main(String[] args) {
+      
+        int num1;           // Declaration
+        num1 = 10;          // Initialization
+        
+        double num2;        // Declaration
+        num2 = 7.86;        // Initialization
+        
+        String name;        // Declaration
+        name = "Dinesh Sharma";  // Initialization
+        // Printing the values of the variables
+        System.out.println("Integer value: " + num1);
+        System.out.println("Double value: " + num2);
+        System.out.println("String value: " + name);
+    }
+}
