71fbea491f6419e2693d7553641e4f3b7a5478ad
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-07-05 01:36:26 +0530

    Create Question1.java

---
+++ PPT Assignments/MockTest2/Question1.java
@@ -0,0 +1,18 @@
+public class Question1 {
+
+    public static void main(String[] args) {
+
+      // 1. - Write a Java program to check whether a given number is positive, negative, or zero using an if-else statement.
+
+      int n = 5;
+        
+        if(n>0) {
+            System.out.println("n is Positive");
+        } else if(n<0) {
+            System.out.println("n is Negative");
+        } else {
+            System.out.println("n is zero");
+        }
+    }
+  
+}
